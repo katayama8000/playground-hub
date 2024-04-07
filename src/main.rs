36ecce8143b55df74cd4cf9ded7e712effe496a1
katayama800 <tattu.0310@gmail.com>
2024-04-07 13:41:25 +0000

    add somple ui

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use log::LevelFilter;
 enum Route {
     #[route("/")]
     Home {},
+    #[route("/playground")]
+    Playground {},
     #[route("/blog/:id")]
     Blog { id: i32 },
 }
@@ -34,6 +36,37 @@ fn Blog(id: i32) -> Element {
 }
 
 #[component]
+fn Playground() -> Element {
+    let programing_language = [
+        ("Rust", "https://www.rust-lang.org"),
+        ("JavaScript", "https://www.javascript.com"),
+        ("TypeScript", "https://www.typescriptlang.org"),
+        ("Python", "https://www.python.org"),
+        ("Java", "https://www.java.com"),
+        ("C++", "https://isocpp.org"),
+        ("C#", "https://docs.microsoft.com/en-us/dotnet/csharp/"),
+        ("PHP", "https://www.php.net"),
+        ("Swift", "https://developer.apple.com/swift/"),
+        ("Go", "https://golang.org"),
+    ];
+
+    rsx! {
+        div {
+            h1 { "Playground" }
+            ul { style: "list-style-type: none; padding: 0; margin: 0;",
+                for (lang, url) in programing_language.iter() {
+                    li { style: "padding: 0.5rem; margin: 0.5rem; background-color: #f3f4f6;",
+                        a { href: *url, "{lang}" }
+                    }
+                }
+            }
+            h1 { style: "color: red; font-size: 2rem; text-align: center;", "CSS" }
+            a { style: "color: blue; text-decoration: none;", href: "https://www.google.com", "Google" }
+        }
+    }
+}
+
+#[component]
 fn Home() -> Element {
     let mut count = use_signal(|| 0);
 
@@ -44,6 +77,7 @@ fn Home() -> Element {
             },
             "Go to blog"
         }
+        Link { to: Route::Playground {}, "Go to playground" }
         div {
             h1 { "High-Five counter: {count}" }
             button { onclick: move |_| count += 1, "Up high!" }
